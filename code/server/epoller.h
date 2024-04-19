02ed591dd0b9ce3dcd9c7048d2d5d70bc57168db
sheriby <hony_sher@foxmail.com>
2024-04-19 19:35:19 +0800

    commit tiny http server

---
+++ code/server/epoller.h
@@ -0,0 +1,36 @@
+#ifndef EPOLLER_H
+#define EPOLLER_H
+
+#include <assert.h>  // close()
+#include <errno.h>
+#include <fcntl.h>      // fcntl()
+#include <sys/epoll.h>  //epoll_ctl()
+#include <unistd.h>     // close()
+
+#include <vector>
+
+class Epoller {
+   public:
+    explicit Epoller(int maxEvent = 1024);
+
+    ~Epoller();
+
+    bool AddFd(int fd, uint32_t events);
+
+    bool ModFd(int fd, uint32_t events);
+
+    bool DelFd(int fd);
+
+    int Wait(int timeoutMs = -1);
+
+    int GetEventFd(size_t i) const;
+
+    uint32_t GetEvents(size_t i) const;
+
+   private:
+    int epollFd_;
+
+    std::vector<struct epoll_event> events_;
+};
+
+#endif  // EPOLLER_H
